c44ff10a817884a0d05cf0575de7351182ad59f6
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-14 19:06:28 +0330

    Add variable declarations to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,15 @@ public class Hello {
     public static void main(String[] args) {
 
 
-        System.out.println("hello world from java");
+        byte by = 2;
+        short sh = 3;
+        long lg = 232321l;
+        double f = 5.8;
+        char c = 'a';
+        boolean b = true;
 
+        
+        System.out.println("hello world from java");
+        
     }
 }
