9c77b62c4f0486111137dd3291d8d8f5bbfc1e0c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:15:52 +0330

    Add constructor overloading to Human class and update Something class to demonstrate usage

--- Something.java
+++ Something.java
@@ -6,6 +6,23 @@ class Human {
     private int age;
     private String name;
 
+    // constructor: a special method that is called when an object is instantiated what it does is to initialize the object of the class 
+    // default constructor
+    public Human()  {
+        age = 34;
+        name = "Snake";
+    }
+
+    
+
+    // constructor overloading
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+
     // public: access modifier means that the method is visible and can be called
     // from other objects of other types
     // encapsulation: the field is private and can only be accessed through a public
@@ -34,7 +51,11 @@ class Human {
 
 public class Something {
     public static void main(String[] args) {
-        Human human = new Human();
+        // everytime the object is created, the constructor is called
+        Human human = new Human(24,"John");
+        Human human2 = new Human();
+
+        System.out.println(human2.getAge() + " " + human2.getName());
         System.out.println(human.getAge() + " " + human.getName());
 
     }
