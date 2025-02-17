6263b8216053139bcd74cf6bd55339926b12371a
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:23:05 +0330

    Add Inheritence class with a simple demonstration of class instantiation and method invocation

---
+++ Inheritence.java
@@ -0,0 +1,13 @@
+class A {
+    public void show() {
+        System.out.println("A");
+    }
+}
+
+public class Inheritence {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+        
+    }
+}
