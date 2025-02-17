6e4345706ded29befa3c8690ecd5d5ac0f7d1f3a
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 17:18:58 +0330

    Refactor Demo class to use a jagged array and populate it with random integers

--- Demo.java
+++ Demo.java
@@ -1,14 +1,22 @@
 public class Demo {
     public static void main(String[] args) {
-        int nums[] = new int[5];
-        nums[0] = 1;
-        nums[1] = 4;
-        nums[2] = 5;
-        nums[3] = 6;
-        nums[4] = 7;
+        int nums[][] = new int[3][]; // jagged array
         
+        // different sizes of an array
+        nums[0] = new int[2];
+        nums[1] = new int[3];
+        nums[2] = new int[4];
+
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = (int) (Math.random() * 10);
+                System.out.println(nums[i][j]);
+            }
+            System.out.println();
         }
+
+       
+        
+
     }
 }
