5e3bd48f2d22410fde03751816a094ada618b098
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 16:51:03 +0330

    Add Demo class with main method to initialize and print an array of integers

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+        int nums[] = new int[5];
+        nums[0] = 1;
+        nums[1] = 4;
+        nums[2] = 5;
+        nums[3] = 6;
+        nums[4] = 7;
+        
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
