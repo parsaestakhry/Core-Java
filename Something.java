f3aedf456fa417d1028becb0a615d61e5b8aa245
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:06:53 +0330

    Refactor Human class for improved encapsulation and clarity; update Something class to demonstrate usage

--- Something.java
+++ Something.java
@@ -1,10 +1,15 @@
 class Human {
-    // private: access modifier means that the field is only accessible within the same class
+    // private: access modifier means that the field is only accessible within the
+    // same class
     // to accsess the field, we need to create a public method in the same class
-    private int age = 20;
-    private String name = "John";
-    // public: access modifier means that the method is visible and can be called from other objects of other types
-    // encapsulation: the field is private and can only be accessed through a public method
+    // instance variables
+    private int age;
+    private String name;
+
+    // public: access modifier means that the method is visible and can be called
+    // from other objects of other types
+    // encapsulation: the field is private and can only be accessed through a public
+    // method
     public int getAge() {
         return age;
     }
@@ -12,23 +17,25 @@ class Human {
     public String getName() {
         return name;
     }
-    
+
+    // age:local variable
     public void setAge(int age) {
+        // this: keyword refers to the current object means that the field is assigned
+        // the value of the parameter and not the other way around
+        // we are specifying that the field is assigned the value of the parameter
         this.age = age;
     }
 
     public void setName(String name) {
         this.name = name;
     }
-    
+
 }
+
 public class Something {
     public static void main(String[] args) {
         Human human = new Human();
-        System.out.println(human.getAge());
-        System.out.println(human.getName());
-        human.setAge(30);
-        System.out.println(human.getAge());
-        
+        System.out.println(human.getAge() + " " + human.getName());
+
     }
 }
