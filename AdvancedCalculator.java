e8dbc7f4d27d21527ef0529131e6605c12f45ea8
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:46:37 +0330

    Add AdvancedCalculator class with multiply and divide methods

---
+++ AdvancedCalculator.java
@@ -0,0 +1,10 @@
+public class AdvancedCalculator extends Calc{
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+}
