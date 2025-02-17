858b98ed71fe8d793ef9512f58493d9af152fcb2
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:26:55 +0330

    Add constructor to class A and demonstrate usage in Inheritence class

--- Inheritence.java
+++ Inheritence.java
@@ -2,11 +2,18 @@ class A {
     public void show() {
         System.out.println("A");
     }
+
+    public A() {
+        System.out.println("Constructor A");
+    }
 }
 
 public class Inheritence {
     public static void main(String[] args) {
+        // reference variable
         A a = new A();
+
+        new A(); // anonymous object
         a.show();
         
     }
