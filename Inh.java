b17bebdd8914f3283c2a5125cc0f6ab74050ca70
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-17 20:46:48 +0330

    Add Inh class with calculator functionality and remove Inheritence class

---
+++ Inh.java
@@ -0,0 +1,26 @@
+public class Inh {
+
+    public static void main(String[] args) {
+        Calc calculator = new Calc();
+        AdvancedCalculator advancedCalculator = new AdvancedCalculator();
+        System.out.println(advancedCalculator.add(2, 0));
+        
+       
+    }
+}
+
+
+class Calc {
+    int num;
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+}
