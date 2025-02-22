049836262b98cab8bd46d39c0d0fdffc98927502
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-22 17:49:30 +0330

    Add MultilevelInheritence class with basic structure and class hierarchy

---
+++ MultilevelInheritence.java
@@ -0,0 +1,24 @@
+public class MultilevelInheritence {
+    public static void main(String[] args) {
+        
+
+    }
+}
+
+
+
+class A {
+
+
+}
+
+class B extends A {
+
+}
+
+
+class C extends B {
+
+}
+
+// multiple inheritence is not allowed in java
