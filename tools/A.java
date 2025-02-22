8e2fded6b9579e4985505513aae301d9e48c702c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-22 21:00:31 +0330

    Add classes A and B, and update Hello class to demonstrate usage of A

---
+++ tools/A.java
@@ -0,0 +1,10 @@
+package tools;
+
+public class A {
+    private int marks = 6;
+
+    public int getMarks() {
+        return marks;
+    }
+
+}
--- tools/Hello.java
+++ tools/Hello.java
@@ -1,5 +1,13 @@
 package tools;
 
-public class Hello {
 
+public class Hello {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.getMarks());
+    }
 }
+
+
+// protected: only accessible within the same package or by subclasses
+// default: only accessible within the same package
